1faff56853e8e0a89c1cb01f3bccc85e0966bdc0
santiarroyave <santi98cv@hotmail.com>
2023-08-16 19:43:07 +0200

    Ordenando directorios de proyecto

---
+++ T24-Basic-C-Sharp/M2/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T24_Basic_C_Sharp.M2
+{
+    internal class Main
+    {
+        public void Inicia()
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("##################################################");
+            Console.WriteLine("#                                                #");
+            Console.WriteLine("# M2 - Exercici Lletres Repetides                #");
+            Console.WriteLine("#                                                #");
+            Console.WriteLine("##################################################\n");
+
+            // - MILESTONE 1
+            Console.WriteLine("MILESTONE 1");
+            Console.WriteLine("--------------------------------------------------");
+            Milestone1 Milestone1 = new Milestone1();
+            Milestone1.Inicia();
+        }
+    }
+}
---
+++ T24-Basic-C-Sharp/M2/Milestone1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace T24_Basic_C_Sharp.M2
+{
+    internal class Milestone1
+    {
+        public void Inicia()
+        {
+            Console.WriteLine("hola esto es el m2");
+        }
+    }
+}
